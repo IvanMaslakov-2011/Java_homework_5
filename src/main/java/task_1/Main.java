28cf1108d69904f0ec45be91ec3f2025fef3c556
Ivan Maslakov <0500456367v@gmail.com>
2026-04-22 22:04:33 +0300

    First task

--- src/main/java/task_1/Main.java
+++ src/main/java/task_1/Main.java
@@ -7,23 +7,23 @@ public class Main {
         SimpleCalculator simpleCalc = new SimpleCalculator();
         AdvancedCalculator advancedCalc = new AdvancedCalculator();
 
-        System.out.println("Результат додавання: " + simpleCalc.add(44, 55));
-        System.out.println("Результат віднімання: " + simpleCalc.subtract(77, -78));
-        System.out.println("Результат множення: " + simpleCalc.multiply(45, 10));
+        System.out.println("Результат додавання: " + simpleCalc.add(44, 55)); //  Метод add з SimpleCalc
+        System.out.println("Результат віднімання: " + simpleCalc.subtract(77, -78)); // Метод substract з SimpleCalc
+        System.out.println("Результат множення: " + simpleCalc.multiply(45, 10)); // Метод multiply з SimpleCalc
         try {
-        System.out.println("Результат ділення: " + simpleCalc.divide(12, 0));
+        System.out.println("Результат ділення: " + simpleCalc.divide(12, 0)); // метод divide з SimpleCalc
         } catch (ArithmeticException e) {
         System.out.println("Помилка! Ділення на нуль!)");
     }
 
 
-        System.out.println("Результат піднесення до степеня: " + advancedCalc.power(5, 3));
+        System.out.println("Результат піднесення до степеня: " + advancedCalc.power(5, 3)); // Метод power з AdvancedCalc
         try {
-            System.out.println("Факторіал числа: " + advancedCalc.factorial(20));
+            System.out.println("Факторіал числа: " + advancedCalc.factorial(20)); // Метод factorial з AdvancedCalc
         } catch (ArithmeticException e){
             System.out.println("Помилка! " + e.getMessage());
         }
 
-        System.out.println("Число просте? " + advancedCalc.isEven(17));
+        System.out.println("Число просте? " + advancedCalc.isEven(17)); // Метод isEven з AdvancedCalc
     }
 }
